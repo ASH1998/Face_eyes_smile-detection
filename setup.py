d95365a90f62220968b6bbc54a9cf639820830d2
Ashutosh Mishra <mishra06ashutosh@gmail.com>
2017-03-14 16:57:04 +0530

    This is the setup file to make this a stand alone program

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+# THIS SCRIPT SHOULD BE CALLED SETUP.PY
+import cx_Freeze 
+
+executables = [
+        #                   name of your game script
+        cx_Freeze.Executable("FACE_EYES_SMILE_DETECTION.py") 
+] 
+cx_Freeze.setup( 
+        name = "Face, Eyes & Smile Detection",
+        version='0.1',
+        options= {"build_exe": {"packages":["numpy", 'cv2'],
+                                "include_files":["eye.xml", "smile.xml", "frontalface_default.xml"]}},
+        description = "Must have a web-cam to see your face!!!", 
+        executables = executables)
+
+
+#python setup.py build
+#python setup.py bdist_msi
